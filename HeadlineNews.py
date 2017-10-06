9cbbfd7164686776a3bffb811e37c9c90c1a8781
chenchuanlong <chenchuanlong@hotmail.com>
2017-10-06 21:29:13 +0800

    改进新闻插件 (#30)

--- HeadlineNews.py
+++ HeadlineNews.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# 新闻头条插件
+# 新闻插件
 import sys
 import os
 import logging
@@ -9,14 +9,14 @@ from urllib import urlencode
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-WORDS = ["TOUTIAOXINWEN"]
+WORDS = ["XINWEN"]
 SLUG = "headline_news"
 
-def request(appkey, mic, logger, m="GET"):
+def request(appkey,type, mic, logger, m="GET"):
     url = "http://v.juhe.cn/toutiao/index"
     params = {
         "key" : appkey,
-        "type" : "top",
+        "type" : type[1]
     }
     params = urlencode(params)
     if m == "GET":
@@ -29,6 +29,7 @@ def request(appkey, mic, logger, m="GET"):
     if res:
         error_code = res["error_code"]
         if error_code == 0:
+            mic.say(type[0] + u"新闻")
             limit = 5;
             news = res["result"]["data"][0:limit]
             news_for_tts = ""
@@ -39,18 +40,27 @@ def request(appkey, mic, logger, m="GET"):
             logger.error(str(error_code) + ':' + res["reason"])
             mic.say(res["reason"])
     else:
-        mic.say(u"新闻头条接口调用错误")
+        mic.say(u"新闻接口调用错误")
 
+def getNewsType(text):
+    newsTypes = {"头条":"top", "社会":"shehui","国内":"guonei", "国际":"guoji", "娱乐":"yule",
+                 "体育":"tiyu", "军事":"junshi", "科技":"keji","财经":"caijing","时尚":"shishang"}
+    newsType = ["头条","top"]
+    for type in newsTypes:
+        if type  in text:
+            newsType = [type,newsTypes[type]]
+    return  newsType
 
 def handle(text, mic, profile, wxbot=None):
     logger = logging.getLogger(__name__)
 
     if SLUG not in profile or \
        'key' not in profile[SLUG]:
-        mic.say(u"新闻头条插件配置有误，插件使用失败")
+        mic.say(u"新闻插件配置有误，插件使用失败")
         return
     key = profile[SLUG]['key']
-    request(key, mic, logger)
+    type = getNewsType(text)
+    request(key,type, mic, logger)
 
 def isValid(text):
-    return any(word in text for word in [u"新闻头条", u"头条新闻", u"头条"])
+    return u"新闻" in text
